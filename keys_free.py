2a2bd35983d843b5de014f980346e47135bcdfc1
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2025-02-02 15:01:37 +0200

    Добавил keys_free

---
+++ keys_free.py
@@ -0,0 +1,12 @@
+TELEGRAM_BOT_TOKEN = "your_key_bot"
+CHAT_ID = "@your_chanel"
+OWNER_USERNAME = "@your_name"  # Имя пользователя владельца канала
+
+# API-ключи
+OPENAI_API_KEY = "your_key_openai"
+PEXELS_API_KEY = "your_key_pexels"  # Вставьте сюда ваш API-ключ
+GOOGLE_API_KEY = "your_key_google"  # Вставьте сюда ваш API-ключ
+SEARCH_ENGINE_ID = "your_key_CX"  # Вставьте сюда идентификатор поисковой системы (CX)
+
+# Файл для хранения статистики
+reaction_data_file = "reaction_data.json"
